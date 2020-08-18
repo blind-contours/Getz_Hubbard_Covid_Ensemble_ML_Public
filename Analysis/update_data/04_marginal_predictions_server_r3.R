11f68091f9304598492548c588bbf12549ad30a3
David McCoy <david_mccoy@berkeley.edu>
2020-08-18 11:08:31 -0700

    r3 to total cases by prop black individuals boot.

--- Analysis/update_data/04_marginal_predictions_server_r3.R
+++ Analysis/update_data/04_marginal_predictions_server_r3.R
@@ -356,27 +356,40 @@ registerDoParallel(ncores)
 
 start_time <- Sys.time()
 
-boot_results <- purrr::pmap(list(
-  target_variable = top_vars,
-  ML_pipeline_result = ML_pipeline_results,
-  outcome = target_outcomes,
-  boot_df_sf_full = boot_dfs_sl_full,
-  boot_df_sf_no_subcat = boot_dfs_sl_no_subcat,
-  boot_df_univar_gam = boot_dfs_univar_gam,
-  sub_cat_vars = top_var_subcat_vars
-),
-.f = bootstrap_marginal_predictions,
-data_original = data_original,
-covars = covars,
-percents = percents,
-pop = data_original$Population,
-boot_num = 300
-)
+# boot_results <- purrr::pmap(list(
+#   target_variable = top_vars,
+#   ML_pipeline_result = ML_pipeline_results,
+#   outcome = target_outcomes,
+#   boot_df_sf_full = boot_dfs_sl_full,
+#   boot_df_sf_no_subcat = boot_dfs_sl_no_subcat,
+#   boot_df_univar_gam = boot_dfs_univar_gam,
+#   sub_cat_vars = top_var_subcat_vars
+# ),
+# .f = bootstrap_marginal_predictions,
+# data_original = data_original,
+# covars = covars,
+# percents = percents,
+# pop = data_original$Population,
+# boot_num = 300
+# )
+
+total_cases_by_prop_black_boot_results <- bootstrap_marginal_predictions(target_variable = "pct_black_only_2018", 
+                                                                       ML_pipeline_result = ML_pipeline_results[[3]],
+                                                                       outcome = target_outcomes[[3]],
+                                                                       boot_df_sf_full = boot_dfs_sl_full[[5]],
+                                                                       boot_df_sf_no_subcat = boot_dfs_sl_no_subcat[[5]],
+                                                                       boot_df_univar_gam = boot_dfs_univar_gam[[5]],
+                                                                       sub_cat_vars = top_var_subcat_vars[[5]],
+                                                                       data_original = data_original,
+                                                                       covars = covars,
+                                                                       percents = percents,
+                                                                       pop = data_original$Population,
+                                                                       boot_num = 1000)
 
 end_time <- Sys.time()
 end_time - start_time
 
-saveRDS(boot_results, here("Analysis/update_data/data/processed/BootResults_Aug_8_run3.RDS"))
+saveRDS(total_cases_by_prop_black_boot_results, here("Analysis/update_data/data/processed/total_cases_by_prop_black_boot_results.RDS"))
 
 stopCluster(cl)
 
